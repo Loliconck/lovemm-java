b5fadb82a47c5c7e55ca9db861e55becf656a49b
loliconck <loliconck@gmail.com>
2018-09-08 10:51:58 +0800

    initial

---
+++ src/main/java/com/loliconck/lovemm/LovemmApplication.java
@@ -0,0 +1,12 @@
+package com.loliconck.lovemm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovemmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovemmApplication.class, args);
+    }
+}
